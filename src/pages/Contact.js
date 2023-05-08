f4083f66849ed5619b115a16135795dded7f32a5
HackerGuy1000 <tyler.j.philip22@gmail.com>
2023-05-08 11:37:03 -0400

    Added CSS to Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,15 @@
 import Header from "../components/Header";
+import "../components/css/Main.css"
+
+document.body.style = 'background: #e0f9fb;';
 
 export default function Contact() {
     return(
         <>
         <Header/>
+        <section className="main">
         <h2>Contact</h2>
+        </section>
         </>
     )
 }
