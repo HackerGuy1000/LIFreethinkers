98daecb616b69e61657002729e56869c89f399be
HackerGuy1000 <tyler.j.philip22@gmail.com>
2023-05-08 11:37:14 -0400

    Added Events Page

---
+++ src/pages/Events.js
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+import Event from "../components/Event";
+import Meeting1 from "../components/images/meeting.jpeg"
+import "../components/css/Main.css"
+
+
+document.body.style = 'background: #e0f9fb;';
+
+export default function Events() {
+    return(
+        <>
+        <Header/>
+        <section className="main">
+        <h2>Events</h2>
+        </section>
+        <Event image={Meeting1} title="First Introductory Event" text="For our first event we are gonna bruh you know"/>
+        </>
+    )
+}
