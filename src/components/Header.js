d8c33b782934c1fcf896fff194a0d9a5cdb271a5
HackerGuy1000 <tyler.j.philip22@gmail.com>
2023-05-08 11:31:48 -0400

    Added links and logo to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
 import {React} from "react";
 import "./css/Header.css"
+import logo from './images/LIFT.png'
+
 export default function Header() {
     return (
-        <section class="top-nav">
-            <div>
-                Logo Here
+        <section className="top-nav">
+            <div id="logo">
+                <a className="nostyle" href="/home"><img src={logo} width={40} height={40} /></a>
             </div>
             <input id="menu-toggle" type="checkbox" />
-            <label class='menu-button-container' for="menu-toggle">
-                <div class='menu-button'></div>
+            <label className='menu-button-container' htmlFor="menu-toggle">
+                <div className='menu-button'></div>
             </label>
-            <ul class="menu">
-                <li>Events</li>
-                <li>Contact</li>
-                <li>About</li>
+            <ul className="menu">
+                <li><a className="nostyle" href="/events">Events</a></li>
+                <li><a className="nostyle" href="/contact">Contact</a></li>
+                <li><a className="nostyle" href="/about">About</a></li>
             </ul>
         </section>
     )
