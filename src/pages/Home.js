6239263643920ba78f0c3e6fafc2742555ca765d
HackerGuy1000 <tyler.j.philip22@gmail.com>
2023-05-08 11:37:27 -0400

    Added styling to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import Header from "../components/Header";
+import Footnote from "../components/Footnote";
+import "../components/css/Main.css"
+
+document.body.style = 'background: #e0f9fb;';
 
 export default function Home() {
     return(
         <>
         <Header/>
-        <h2>Homepage</h2>
+        <section className="main">
+        <h1 id="title">Long Island Free Thinkers</h1>
+        </section>
         </>
     )
 }
