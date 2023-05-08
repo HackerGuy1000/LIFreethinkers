c37a28d41bc4a6b6a63acad859a44a978c572dd7
HackerGuy1000 <tyler.j.philip22@gmail.com>
2023-05-08 11:31:23 -0400

    Created footnote component

---
+++ src/components/Footnote.js
@@ -0,0 +1,17 @@
+import {React} from "react";
+import "./css/Footnote.css"
+import logo from './images/LIFT.png'
+
+export default function Footnote() {
+    return (
+        <center>
+                <div id="container">
+                
+                <div id="footer">
+                    <p></p>
+                </div>
+                </div>
+            </center>
+    
+    )
+}
