45e70f042901d22a8d65ec4ab44268414479fe19
HackerGuy1000 <tyler.j.philip22@gmail.com>
2023-05-08 11:37:38 -0400

    Added a 404 page

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,10 +1,15 @@
 import Header from "../components/Header";
+import "../components/css/Main.css"
+
+document.body.style = 'background: #e0f9fb;';
 
 export default function NoPage() {
     return(
         <>
         <Header/>
+        <section className="main">
         <h2>Error 404. Not Found</h2>
+        </section>
         </>
     )
 }
