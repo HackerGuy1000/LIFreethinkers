5deb642dca5769f37ab27b2c7d48d2828a594815
HackerGuy1000 <tyler.j.philip22@gmail.com>
2023-07-29 20:46:53 -0400

    Created Components For The About Page

---
+++ src/components/Branches.js
@@ -0,0 +1,12 @@
+import {React} from "react";
+
+export default function Branches(props) {
+    return (
+        <p id= "aboutText">Our current branches include: 
+        <br/> 
+        -Syosset High School
+        <br/>
+        -Wheately High School
+        </p>
+    )
+}
