e082384aced89209f5142dc3588443712b2b59cf
HackerGuy1000 <tyler.j.philip22@gmail.com>
2023-07-31 10:56:46 -0400

    Updated Header For New Pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,14 @@ export default function Header() {
     return (
         <section className="top-nav">
             <div id="logo">
-                <a className="nostyle" href="/home"><img src={logo} width={40} height={40} /></a>
+                <a className="nostyle" href="/home"><img src={logo} width={40} height={40}/> <h1 className="title">LIFT</h1> </a>
             </div>
             <input id="menu-toggle" type="checkbox" />
             <label className='menu-button-container' htmlFor="menu-toggle">
                 <div className='menu-button'></div>
             </label>
             <ul className="menu">
-                <li><a className="nostyle" href="/events">Events</a></li>
+                <li><a className="nostyle" href="/merch">Shop</a></li>
                 {/* <li><a className="nostyle" href="/contact">Contact</a></li> */}
                 <li><a className="nostyle" href="/about">About</a></li>
             </ul>
