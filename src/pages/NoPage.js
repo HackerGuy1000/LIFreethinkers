6a796d5fe86b7a4808af87746011abca3271a177
HackerGuy1000 <tyler.j.philip22@gmail.com>
2023-07-31 19:04:03 -0400

    Updated Title For 404 Page

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,16 +1,14 @@
 import Header from "../components/Header";
-import Footer from "../components/Footer";
 import "../components/css/Main.css"
 
-document.body.style = 'background: #e0f9fb;';
+// document.body.style = 'background: #e0f9fb;';
 
 export default function NoPage() {
     return(
         <>
         <Header/>
         <section className="main">
-        <h2>Error 404. Not Found</h2>
-        <Footer/>
+        <h2 className="page-title">Error 404. Not Found</h2>
         </section>
         </>
     )
