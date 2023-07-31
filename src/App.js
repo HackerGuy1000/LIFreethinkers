815c143d1bf723f1996f1a792c514ba619d6f13d
HackerGuy1000 <tyler.j.philip22@gmail.com>
2023-07-31 12:36:18 -0400

    Removed Unused Pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import About from './pages/About'
-import Contact from './pages/Contact'
 import NoPage from "./pages/NoPage";
 import Merch from "./pages/Merch";
 
@@ -15,7 +14,6 @@ function App() {
           <Route path="/home" element={<Home />}/>
           <Route path="/merch" element={<Merch />}/>
           <Route path="/about" element={<About />}/>
-          {/* <Route path="/contact" element={<Contact />}/> */}
           <Route path="*" element={<NoPage />}/>
         </Routes>
       </BrowserRouter>
